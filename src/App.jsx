1edfd39794ea7d8b33b3487160d8ed04a7f0c2a5
ALI AGA <99884506+mraliaga@users.noreply.github.com>
2025-03-14 16:42:37 +0300

    sth

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,12 @@ import { RiSquareLine } from "react-icons/ri";
 import { IoPersonAdd } from "react-icons/io5";
 //Component import yukaridakiler
 
+import Card from "./components/Card";
+
 function App() {
   // Bilesen icerisinde verileri yonetmek icin state tanimla
+  //kisi isimlerinin ad ve soyadinin ilk harflerini bolmek icin bu veriyi split ile bolduk .Sonrasinda bunlari isim ve soyisim degerlerine aktadir.
+
   const [contatcs, setContacts] = useState([]);
 
   //Sayfa yuklendiginde Apiden verileri al
@@ -18,8 +22,23 @@ function App() {
       .get("http://localhost:3000/contacts")
       .then((res) => setContacts(res.data));
   }, []);
+  const hundleSubmit = (e) => {
+    //Sayfanin yenilenmesini engelle
+    e.preventDefault();
+    //input icerisindeki degere eris
+    const text = e.target[1].value;
 
-  console.log(contatcs);
+    // apiye gonderilecek parametreleri belirle
+
+    const params = {
+      q: text,
+    };
+
+    //inputdan alinan deger neticesinde ilgili veriyi apiden al
+    axios
+      .get("http://localhost:3000/contacts", { params })
+      .then((res) => setContacts(res.data));
+  };
 
   return (
     <div className="app">
@@ -27,20 +46,18 @@ function App() {
       <header>
         <h1>Rehber</h1>
         <div>
-          <form>
+          <form onSubmit={hundleSubmit}>
             <button>
               <LuSearch />
             </button>
             <input type="text" placeholder="kisi aratiniz ..." />
           </form>
-
           <button className="ns">
             <TiThMenuOutline />
           </button>
           <button>
             <RiSquareLine className="ns" />
           </button>
-
           <button className="add">
             <span>
               <IoPersonAdd />
@@ -52,7 +69,7 @@ function App() {
       {/* Main */}
       <main>
         {contatcs.map((contact) => (
-          <Card key={contact.id} />
+          <Card key={contact.id} contact={contact} />
         ))}
       </main>
     </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,51 @@
-const Card = () => {
-  retur(
-    <div>
-      <h1>Card</h1>
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { GrUpdate } from "react-icons/gr";
+import { BsFillTelephoneInboundFill } from "react-icons/bs";
+import { IoMdMail } from "react-icons/io";
+
+const Card = ({ contact }) => {
+  const [name, surname] = contact.name.split(" ");
+
+  // console.log(name);
+  // console.log(surname);
+
+  return (
+    <div className="card">
+      {/* Buttons */}
+      <div className="buttons">
+        <button>
+          <RiDeleteBin6Line />
+        </button>
+        <button>
+          <GrUpdate />
+        </button>
+      </div>
+      {/* Name */}
+      <h1>
+        {name[0]}
+        {surname[0]}
+      </h1>
+      {/*  */}
+      <h3>{contact.name}</h3>
+      {/* Position */}
+      <p>{contact.position}</p>
+      {/* Company */}
+      <p>{contact.company}</p>
+      {/* Bottom */}
+      <div className="bottom">
+        <div>
+          <span>
+            <BsFillTelephoneInboundFill />
+          </span>
+          <span>{contact.phone}</span>
+        </div>
+        <div>
+          <span>
+            <IoMdMail />
+          </span>
+          <span>{contact.email}</span>
+        </div>
+      </div>
     </div>
   );
 };
